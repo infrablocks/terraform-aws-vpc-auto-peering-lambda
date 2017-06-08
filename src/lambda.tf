53083c9500d9c6553ba39f1997670445243b5526
Robert Bagge <rbagge@thoughtworks.com>
2017-06-08 15:50:14 +0100

    Ensure auto peering lambda has been zipped before pushing to AWS.
    Authors: @BaggeRob @tobyclemson

--- src/lambda.tf
+++ src/lambda.tf
@@ -1,4 +1,4 @@
-data "archive_file" "lambda_zip" {
+data "archive_file" "auto_peering_lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda-definitions/auto_peering"
   output_path = "${path.cwd}/build/auto_peering.zip"
@@ -12,4 +12,6 @@ resource "aws_lambda_function" "auto_peering" {
   runtime = "python3.6"
   timeout = 300
   source_code_hash = "${base64sha256(file("${path.cwd}/build/auto_peering.zip"))}"
+
+  depends_on = ["data.archive_file.auto_peering_lambda_zip"]
 }
