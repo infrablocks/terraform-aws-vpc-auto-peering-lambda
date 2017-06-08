6131cab334d87439173f64b2e7c8e40994dbd4ae
Robert Bagge <rbagge@thoughtworks.com>
2017-06-08 15:59:35 +0100

    Correctly reference archive file in lambda configuration.
    Authors: @BaggeRob @tobyclemson

--- src/lambda.tf
+++ src/lambda.tf
@@ -5,13 +5,11 @@ data "archive_file" "auto_peering_lambda_zip" {
 }
 
 resource "aws_lambda_function" "auto_peering" {
-  filename = "${path.cwd}/build/auto_peering.zip"
+  filename = "${data.archive_file.auto_peering_lambda_zip.output_path}"
   function_name = "auto-peering-${var.region}-${var.deployment_identifier}"
   handler = "auto_peering_lambda.peer_vpcs_for"
   role = "${aws_iam_role.auto_peering.arn}"
   runtime = "python3.6"
   timeout = 300
-  source_code_hash = "${base64sha256(file("${path.cwd}/build/auto_peering.zip"))}"
-
-  depends_on = ["data.archive_file.auto_peering_lambda_zip"]
+  source_code_hash = "${data.archive_file.auto_peering_lambda_zip.output_base64sha256}"
 }
