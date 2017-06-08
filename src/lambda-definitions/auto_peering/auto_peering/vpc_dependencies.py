86d479c41b7746ae8b5d1334b0f99a78dec3b3a5
Robert Bagge <rbagge@thoughtworks.com>
2017-06-08 14:30:08 +0100

    Handled case where dependency vpcs does not exist. + logging
    Authors: @tobyclemson @bagge_rob

--- src/lambda-definitions/auto_peering/auto_peering/vpc_dependencies.py
+++ src/lambda-definitions/auto_peering/auto_peering/vpc_dependencies.py
@@ -6,36 +6,45 @@ from auto_peering.vpc_dependency import VPCDependency
 
 class AllVPCs(object):
     def __init__(self, ec2_client):
-        self.all_vpcs = list(ec2_client.vpcs.all())
+        self.all_vpcs = [
+            self.__with_metadata(vpc)
+            for vpc in ec2_client.vpcs.all()
+        ]
+
+    @staticmethod
+    def __with_metadata(vpc):
+        vpc.component = TagCollection(vpc).find_value('Component')
+        vpc.dependencies = TagCollection(vpc).find_values('Dependencies')
+        return vpc
 
     def find_by_id(self, vpc_id):
         return next(
-            vpc
-            for vpc in self.all_vpcs
-            if vpc.id == vpc_id)
+            (vpc
+             for vpc in self.all_vpcs
+             if vpc.id == vpc_id),
+            None)
 
     def find_for_component(self, component):
         return next(
-            vpc
-            for vpc in self.all_vpcs
-            if TagCollection(vpc).find_value('Component') == component)
+            (vpc
+             for vpc in self.all_vpcs
+             if vpc.component == component),
+            None)
 
     def find_dependencies_of(self, vpc):
-        tag_collection = TagCollection(vpc)
-        dependency_components = tag_collection. \
-            find_values('Dependencies')
-
         return [
-            self.find_for_component(component)
-            for component in dependency_components
+            dependency_vpc
+            for dependency_vpc
+            in (self.find_for_component(component)
+                for component in vpc.dependencies)
+            if dependency_vpc is not None
         ]
 
     def find_dependents_of(self, vpc):
         return [
             dependent_vpc
             for dependent_vpc in self.all_vpcs
-            if TagCollection(vpc).find_value('Component')
-               in TagCollection(dependent_vpc).find_values('Dependencies')
+            if vpc.component in dependent_vpc.dependencies
         ]
 
 
@@ -49,11 +58,41 @@ class VPCDependencies(object):
             source, target, self.ec2_client, self.logger)
 
     def resolve_for(self, target_vpc_id):
+        self.logger.debug(
+            "Computing VPC dependencies for VPC with ID: '%s'.",
+            target_vpc_id)
         all_vpcs = AllVPCs(self.ec2_client)
 
         target_vpc = all_vpcs.find_by_id(target_vpc_id)
+        if target_vpc:
+            self.logger.debug(
+                "Found target VPC with ID: '%s', component: '%s' "
+                "and dependencies: '%s'.",
+                target_vpc_id,
+                target_vpc.component,
+                target_vpc.dependencies)
+        else:
+            self.logger.debug(
+                "No VPC found with ID: '%s'. Aborting.", target_vpc_id)
+            return frozenset()
+
         dependency_vpcs = all_vpcs.find_dependencies_of(target_vpc)
+        self.logger.debug(
+            "Found dependency VPCs: [%s]",
+            ', '.join([
+                "'{}':'{}'".format(
+                    dependency_vpc.component,
+                    dependency_vpc.id)
+                for dependency_vpc in dependency_vpcs]))
+
         dependent_vpcs = all_vpcs.find_dependents_of(target_vpc)
+        self.logger.debug(
+            "Found dependent VPCs: [%s]",
+            ', '.join([
+                "'{}':'{}'".format(
+                    dependent_vpc.component,
+                    dependent_vpc.id)
+                for dependent_vpc in dependent_vpcs]))
 
         vpc_pairs = list(zip(repeat(target_vpc), dependency_vpcs)) + \
                     list(zip(dependent_vpcs, repeat(target_vpc)))
--- src/lambda-definitions/auto_peering/test/test_vpc_dependencies.py
+++ src/lambda-definitions/auto_peering/test/test_vpc_dependencies.py
@@ -14,8 +14,6 @@ def tags_for(component, dependencies):
 
 class TestVPCDependencies(unittest.TestCase):
     def test_resolve_dependencies_for_target_vpc(self):
-        self.maxDiff = None
-
         target_vpc_id = "vpc-12345678"
 
         target_vpc = Mock(name="Target VPC")
@@ -56,11 +54,9 @@ class TestVPCDependencies(unittest.TestCase):
              VPCDependency(target_vpc, dependency_vpc2,
                            ec2_client, logger),
              VPCDependency(dependent_vpc, target_vpc,
-                           ec2_client, logger)}
-        )
+                           ec2_client, logger)})
 
     def test_resolves_no_duplicates(self):
-        self.maxDiff = None
         vpc1_id = "vpc-12345678"
 
         vpc1 = Mock(name="VPC 1")
@@ -87,3 +83,163 @@ class TestVPCDependencies(unittest.TestCase):
             resolved_vpc_dependencies,
             {VPCDependency(vpc1, vpc2,
                            ec2_client, logger)})
+
+    def test_logs_found_target_vpc(self):
+        vpc1_id = "vpc-12345678"
+
+        vpc1 = Mock(name="VPC 1")
+        vpc1.id = vpc1_id
+        vpc1.tags = tags_for('thing1', ['thing2'])
+
+        vpc2 = Mock(name="VPC 2")
+        vpc2.tags = tags_for('thing2', [])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter(
+                [vpc1,
+                 vpc2]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        vpc_dependencies.resolve_for(vpc1_id)
+
+        logger.debug.assert_any_call(
+            "Computing VPC dependencies for VPC with ID: '%s'.", vpc1.id)
+        logger.debug.assert_any_call(
+            "Found target VPC with ID: '%s', component: '%s' "
+            "and dependencies: '%s'.",
+            vpc1.id, 'thing1', ['thing2'])
+
+    def test_logs_not_found_target_vpc(self):
+        vpc1_id = "vpc-12345678"
+
+        vpc1 = Mock(name="VPC 1")
+        vpc1.id = vpc1_id
+        vpc1.tags = tags_for('thing1', ['thing2'])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter([]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        vpc_dependencies.resolve_for(vpc1_id)
+
+        logger.debug.assert_any_call(
+            "No VPC found with ID: '%s'. Aborting.", vpc1.id)
+
+    def test_resolves_empty_set_for_missing_target_vpc(self):
+        vpc1_id = "vpc-12345678"
+
+        vpc1 = Mock(name="VPC 1")
+        vpc1.id = vpc1_id
+        vpc1.tags = tags_for('thing1', ['thing2'])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter([]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        resolved_vpc_dependencies = vpc_dependencies.resolve_for(vpc1_id)
+
+        self.assertEqual(resolved_vpc_dependencies, set())
+
+    def test_ignores_missing_dependencies(self):
+        vpc1_id = "vpc-12345678"
+
+        vpc1 = Mock(name="VPC 1")
+        vpc1.id = vpc1_id
+        vpc1.tags = tags_for('thing1', ['thing2', 'thing3'])
+
+        vpc2 = Mock(name="VPC 2")
+        vpc2.tags = tags_for('thing2', [])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter(
+                [vpc1,
+                 vpc2]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        resolved_vpc_dependencies = vpc_dependencies.resolve_for(vpc1_id)
+
+        self.assertEqual(len(resolved_vpc_dependencies), 1)
+        self.assertEqual(
+            resolved_vpc_dependencies,
+            {VPCDependency(vpc1, vpc2, ec2_client, logger)})
+
+    def test_logs_dependency_vpcs(self):
+        target_vpc_id = "vpc-12345678"
+
+        target_vpc = Mock(name="Target VPC")
+        target_vpc.id = target_vpc_id
+        target_vpc.tags = tags_for('thing1', ['thing2', 'thing3'])
+
+        dependency_vpc1 = Mock(name="Dependency VPC 1")
+        dependency_vpc1.id = "vpc-d1a1a1a1"
+        dependency_vpc1.tags = tags_for('thing2', [])
+
+        dependency_vpc2 = Mock(name="Dependency VPC 2")
+        dependency_vpc2.id = "vpc-d2a2a2a2"
+        dependency_vpc2.tags = tags_for('thing3', [])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter(
+                [dependency_vpc1,
+                 target_vpc,
+                 dependency_vpc2]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        vpc_dependencies. \
+            resolve_for(target_vpc_id)
+
+        logger.debug.assert_any_call(
+            "Found dependency VPCs: [%s]",
+            "'thing2':'vpc-d1a1a1a1', 'thing3':'vpc-d2a2a2a2'")
+
+    def test_logs_dependent_vpcs(self):
+        target_vpc_id = "vpc-12345678"
+
+        target_vpc = Mock(name="Target VPC")
+        target_vpc.id = target_vpc_id
+        target_vpc.tags = tags_for('thing1', [])
+
+        dependent_vpc1 = Mock(name="Dependent VPC 1")
+        dependent_vpc1.id = "vpc-d1a1a1a1"
+        dependent_vpc1.tags = tags_for('thing2', ['thing1'])
+
+        dependent_vpc2 = Mock(name="Dependent VPC 2")
+        dependent_vpc2.id = "vpc-d2a2a2a2"
+        dependent_vpc2.tags = tags_for('thing3', ['thing1'])
+
+        ec2_client = Mock(name="EC2 client")
+        logger = Mock(name="Logger")
+
+        ec2_client.vpcs.all = Mock(
+            name="All VPCs",
+            return_value=iter(
+                [dependent_vpc1,
+                 target_vpc,
+                 dependent_vpc2]))
+
+        vpc_dependencies = VPCDependencies(ec2_client, logger)
+        vpc_dependencies.resolve_for(target_vpc_id)
+
+        logger.debug.assert_any_call(
+            "Found dependent VPCs: [%s]",
+            "'thing2':'vpc-d1a1a1a1', 'thing3':'vpc-d2a2a2a2'")
