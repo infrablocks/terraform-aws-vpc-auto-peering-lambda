055f95213b3c9e108d9c14d1b2d6c5aa4b20dd02
Robert Bagge <rbagge@thoughtworks.com>
2017-06-08 15:03:29 +0100

    Added logic to lambda to create peering connections for target vpc
    Authors: @tobyclemson @bagge_rob

--- src/lambda-definitions/auto_peering/auto_peering/vpc_peering_lambda.py
+++ src/lambda-definitions/auto_peering/auto_peering/vpc_peering_lambda.py
@@ -1,21 +1,49 @@
+import boto3
+import logging
+import json
+
+logging.getLogger('boto3').setLevel(logging.WARNING)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+from auto_peering.s3_event_sns_message import S3EventSNSMessage
+from auto_peering.vpc_links import VPCLinks
+
 
 def peer_vpcs_for(event, context):
-    # 1. get provisioned vpc from event
-    # 2. find all vpcs that depends on the vpc
-    # 3. create peering connections for all combinations
-    # 4. create routes for both sides in all combinations
+    logger.debug('Processing event: {}'.format(json.dumps(event)))
+    ec2_client = boto3.resource('ec2')
 
+    s3_event_sns_message = S3EventSNSMessage(event)
+    target_vpc_id = s3_event_sns_message.target()
+    action = s3_event_sns_message.action()
+    logger.info(
+        "'%s'ing peering connections for '%s'.",
+        action,
+        target_vpc_id)
 
-    # log.ingo('what are we doing?')
-    # log.info('looking up dependencies for: ...')
-    vpc_dependencies = VPCDependencies(ec2_client, logger)
-    vpc_dependencies_for_target = \
-        vpc_dependencies.resolve_for(event.target)
-    # log.info('found dependencies')
-    # for dependency in all_dependencies:
-    #     log.info('establishing peering relationship')
-    #     dependency.peering_relationship.perform(event.action)
-    #     log.info('configuring routes')
-    #     dependency.peering_routes.perform(event.action)
+    vpc_links = VPCLinks(ec2_client, logger)
+    logger.info(
+        "Looking up VPC links for VPC with ID: '%s'.",
+        target_vpc_id)
 
-    return None
+    vpc_links_for_target = vpc_links.resolve_for(target_vpc_id)
+    logger.info(
+        "Found %d VPC links for VPC with ID: '%s'.",
+        len(vpc_links_for_target), target_vpc_id)
+
+    for vpc_link in vpc_links_for_target:
+        logger.info(
+            "Establishing peering relationship"
+            "between '%s' and '%s'.",
+            vpc_link.vpc1.id,
+            vpc_link.vpc2.id)
+        vpc_link.peering_relationship.perform(action)
+
+        logger.info(
+            "Creating peering routes"
+            "between '%s' and '%s'.",
+            vpc_link.vpc1.id,
+            vpc_link.vpc2.id)
+        vpc_link.peering_routes.perform(action)
