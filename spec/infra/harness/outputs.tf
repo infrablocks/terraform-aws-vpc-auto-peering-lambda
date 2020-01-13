25a454eb00bc6743a77f2f969a4bfcfe8ff17caa
Toby Clemson <tobyclemson@gmail.com>
2020-01-13 23:50:57 +0000

    Set multi-account environment variables and update role.

---
+++ spec/infra/harness/outputs.tf
@@ -0,0 +1,3 @@
+output "lambda_role_arn" {
+  value = module.vpc_auto_peering_lambda.lambda_role_arn
+}
--- spec/infra/harness/variables.tf
+++ spec/infra/harness/variables.tf
@@ -1,2 +1,13 @@
 variable "region" {}
 variable "deployment_identifier" {}
+
+variable "search_regions" {
+  type = list(string)
+}
+variable "search_accounts" {
+  type = list(string)
+}
+variable "peering_role_name" {}
+variable "assumable_roles" {
+  type = list(string)
+}
