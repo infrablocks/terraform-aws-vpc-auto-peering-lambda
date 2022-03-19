43006b3f34d05865de1570ff622e831a257758c6
Toby <tobyclemson@gmail.com>
2022-03-19 20:50:58 +0000

    Don't install pyenv-openssl.

--- scripts/ci/common/install-pyenv.sh
+++ scripts/ci/common/install-pyenv.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install -y --no-install-recommends \
     make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
     libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils \
-    tk-dev libffi-dev liblzma-dev python-openssl git
+    tk-dev libffi-dev liblzma-dev git
 
 curl https://pyenv.run | bash
 
